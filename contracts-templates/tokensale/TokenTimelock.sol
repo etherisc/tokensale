26531203ba673c231cbb3cfd3522e5059da0724d
Christoph Mussenbrock <christoph@etherisc.com>
2017-08-28 06:41:20 +0200

    Fixed bug

--- contracts-templates/tokensale/TokenTimelock.sol
+++ contracts-templates/tokensale/TokenTimelock.sol
@@ -10,16 +10,19 @@
 pragma solidity @@include('./util/snippets/solidity_version_string.txt');
 
 contract TokenTimelock is TokenStakeERC20, Ownable {
+  using SafeMath for uint256;
 
   mapping (address => mapping(uint256 => uint256)) releaseTime;
 
   function setTimelock(address _sender, address _staker, uint256 _releaseTime, uint256 _value) {
-  	if (stake(_sender, _staker, _value)) {
-	  	releaseTime[_staker][_releaseTime] = _value;
-  	}
+    if (stake(_sender, _staker, _value)) {
+      releaseTime[_staker][_releaseTime].add(_value);
+    }
   }
 
   function release(address _staker, uint256 _value) {
-    if (releaseTime[_staker][now] >= _value) super.release(_staker, _value)
+    releaseTime[_staker][now].sub(_value); // will throw if result < 0
+    super.release(_staker, _value);
   }
+
 }
