4d94746ec7ffba6ec9448d28445bad86ad664f5c
Kanstantsin Andryanau <andrianov.dev@yandex.by>
2018-04-24 20:42:07 +0300

    cicd

--- ci-cd/travis-testrpc.sh
+++ ci-cd/travis-testrpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-echo "Start Testrpc"
-npm run testrpc > testrpc.log &
+echo "Start Ganache"
+npm run ganache > ganache.log &
 
 echo "Select resources"
 npm run select-resources
